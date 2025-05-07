adaa8d563a78959a58f9c9a4ab6193a2f732952b
Hasan Çiçek <hasancicek2001ist@gmail.com>
2025-05-07 11:35:18 +0300

    Admin paneli tasarımı eklendi
    Tasarım tarafı view componentlere ayırıldı.
    
    Category ve Product entity lerindeki layoutlara admin tasarımı eklendi.

---
+++ RealHouzing/RealHouzing.Consume/ViewComponents/AdminLayout/_AdminFooterPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealHouzing.Consume.ViewComponents.AdminLayout;
+
+public class _AdminFooterPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ RealHouzing/RealHouzing.Consume/ViewComponents/AdminLayout/_AdminSidebarPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealHouzing.Consume.ViewComponents.AdminLayout;
+
+public class _AdminSidebarPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
